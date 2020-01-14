2be10a54ec1a13b4680d856d1565c15ba07e5807
Steven Xu <gazeing@gmail.com>
2020-01-14 21:17:03 +1100

    export properties to email as json string

--- app/src/main/java/com/blackseal/propertyinspector/ui/MainActivity.kt
+++ app/src/main/java/com/blackseal/propertyinspector/ui/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.blackseal.propertyinspector.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -13,16 +17,24 @@ import androidx.navigation.findNavController
 import com.blackseal.propertyinspector.R
 import com.blackseal.propertyinspector.model.Inspection
 import com.blackseal.propertyinspector.ui.EditFragment.Companion.KEY_ID
-import com.blackseal.propertyinspector.ui.dummy.DummyContent
-
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), InspectionListFragment.OnListFragmentInteractionListener {
 
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var view: EditText
+    val gson = Gson()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        view = EditText(this)
+        view.hint = getString(R.string.email)
+        viewModel = ViewModelProviders.of(this)[MainActivityViewModel::class.java]
+        viewModel.inspectionListLiveData.observe(this, Observer {
+            sendEmail(view.text.toString(), gson.toJson(it))
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,7 +49,43 @@ class MainActivity : AppCompatActivity(), InspectionListFragment.OnListFragmentI
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.action_export -> exportProperty()
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun exportProperty(): Boolean {
+
+        AlertDialog.Builder(this).apply {
+            title?.let { setTitle(it) }
+            setCancelable(false)
+            setView(view)
+            setPositiveButton(R.string.ok) { _, _ ->
+                viewModel.export(view.text.toString())
+            }
+
+            setNegativeButton(R.string.cancel) { _, _ ->
+
+            }
+
+        }.show()
+        return true
+    }
+
+    fun sendEmail(email: String, content: String) {
+        val i = Intent(Intent.ACTION_SEND)
+        i.type = "message/rfc822"
+        i.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        i.putExtra(Intent.EXTRA_SUBJECT, "Property Backup")
+        i.putExtra(Intent.EXTRA_TEXT, content)
+        try {
+            startActivity(Intent.createChooser(i, "Send report mail..."))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            Toast.makeText(
+                this,
+                "There are no email clients installed.",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -51,5 +99,28 @@ class MainActivity : AppCompatActivity(), InspectionListFragment.OnListFragmentI
 
     private fun getNavigationController(): NavController {
         return findNavController(R.id.my_nav_host_fragment)
+    }
+}
+
+fun Context.showNormalConfirmDialog(
+    title: String?, msg: String, callback: () -> Unit,
+    cancelCallback: (() -> Unit)? = null,
+    okTextRes: Int = R.string.ok,
+    cancelTextRes: Int = R.string.cancel
+) {
+    AlertDialog.Builder(this).apply {
+        title?.let { setTitle(it) }
+        setCancelable(false)
+        setMessage(msg)
+        setPositiveButton(okTextRes) { _, _ ->
+            callback()
+        }
+        if (cancelCallback != null) {
+            setNegativeButton(cancelTextRes) { _, _ ->
+                cancelCallback()
+            }
+        }
+    }.let {
+        it.show()
     }
 }
--- app/src/main/java/com/blackseal/propertyinspector/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/blackseal/propertyinspector/ui/MainActivityViewModel.kt
@@ -2,13 +2,24 @@ package com.blackseal.propertyinspector.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.blackseal.propertyinspector.data.AppDatabase
 import com.blackseal.propertyinspector.data.InspectionRepo
+import com.blackseal.propertyinspector.model.Inspection
 
 class MainActivityViewModel(application: Application):AndroidViewModel(application) {
     private val database = AppDatabase.getInstance(application)
     private val repo = InspectionRepo(database.inspectionListDao())
 
-    fun loadList() = repo.loadList()
+    val idLiveData = MutableLiveData<String>()
+
+    val inspectionListLiveData: LiveData<List<Inspection>> = idLiveData.switchMap { id ->
+        repo.loadList()
+    }
+
+    fun export(email:String){
+        idLiveData.value = email
+    }
 
 }
